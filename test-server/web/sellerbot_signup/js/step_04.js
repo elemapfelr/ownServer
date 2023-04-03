b0148250e55d621019339cb46c655757031f8596
Junhyeok <elemapfelr@gmail.com>
2023-04-03 12:45:31 +0900

    파이봇할인형 문구 변경 및 step_04 계좌등록 mp4추가

--- test-server/web/sellerbot_signup/js/step_04.js
+++ test-server/web/sellerbot_signup/js/step_04.js
@@ -8,13 +8,13 @@ document.querySelector('#mallReg').addEventListener('click', () => {
 	document.querySelector('#mallReg').classList.add('active');
 	document.querySelector('#accountReg').classList.remove('active');
 	document.querySelector('.mallRegVid').classList.add('active');
-	document.querySelector('.accRegImg').classList.remove('active');
+	document.querySelector('.accRegVid').classList.remove('active');
 });
 document.querySelector('#accountReg').addEventListener('click', () => {
 	document.querySelector('#mallReg').classList.remove('active');
 	document.querySelector('#accountReg').classList.add('active');
 	document.querySelector('.mallRegVid').classList.remove('active');
-	document.querySelector('.accRegImg').classList.add('active');
+	document.querySelector('.accRegVid').classList.add('active');
 });
 
 document.querySelector('#nextStep').addEventListener('click', () => {
